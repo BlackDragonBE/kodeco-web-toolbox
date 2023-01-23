97dcb72c1b8b7d7ce5cb63e98330d13cccfaeebb
Eric Van de Kerckhove <blackdragon.be@gmail.com>
2023-01-23 19:31:37 +0100

    Added showdown + first converter draft

---
+++ src/lib/MdToHtml.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Converter, setOption } from 'showdown';
+
+  var text = '';
+  var html = '';
+
+  const convert = () => {
+    var converter = new Converter();
+    converter.setOption('noHeaderId', true);
+    converter.setFlavor('github');
+    html = converter.makeHtml(text);
+	html = html.replaceAll('<em>', '<i>');
+	html = html.replaceAll('</em>', '</i>');
+  };
+</script>
+
+<textarea id="md" name="md" rows="10" cols="100" bind:value={text} />
+<p />
+<textarea id="html" name="html" rows="10" cols="100" bind:value={html} />
+<p />
+<button on:click={convert}> Convert </button>
